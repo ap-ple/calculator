9f96eecb1f63fa15afa5ed24e3bcf5c6910f213b
Durish <jdurish2@gmail.com>
2025-02-06 11:45:16 -0500

    Add input functionality

--- script.js
+++ script.js
@@ -16,3 +16,42 @@ let operator;
 let left;
 let right;
 
+let screen = document.getElementById("screen");
+
+const setScreenText = (newText) => {
+   screen.innerText = newText;
+   left = newText;
+} 
+
+let buttons = Array.from(document.getElementsByTagName("button"));
+
+buttons.forEach(button => {
+   // if digit button, add to display
+   if (button.innerText.match(/\d/)) {
+      button.addEventListener("click", event => {
+         if (screen.innerText === "0") {
+            // input is empty; set new input
+            setScreenText(event.target.innerText);
+         } else {
+            setScreenText(screen.innerText + event.target.innerText);
+         }
+      });
+   }
+   else if (button.innerText === "CE") {
+      button.addEventListener("click", event => {
+         setScreenText("0");
+      });
+   }
+   else if (button.innerText === "←") {
+      button.addEventListener("click", event => {
+         if (screen.innerText.length === 1) {
+            if (screen.innerText !== "0") {
+               setScreenText("0");
+            }
+         }
+         else {
+            setScreenText(screen.innerText.slice(0, -1));
+         }
+      });
+   }
+});
